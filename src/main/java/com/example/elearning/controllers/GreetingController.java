9fc2f4ff686e9702de09e908a7c2faf678c9315b
dlimiyassin <dlimiyassine08@gmail.com>
2024-02-21 20:03:52 +0100

    first commit

---
+++ src/main/java/com/example/elearning/controllers/GreetingController.java
@@ -0,0 +1,14 @@
+package com.example.elearning.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    @GetMapping()
+    public String greeting(){
+        return "welcome my friend!";
+    }
+}
